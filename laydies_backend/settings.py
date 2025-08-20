d8eb206f898f15c0bbe5caad26ef5f8623c01d66
IsaacParkire <pixieparkire@gmail.com>
2025-08-20 21:14:39 +0300

    changes for render

--- laydies_backend/settings.py
+++ laydies_backend/settings.py
@@ -186,6 +186,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://isaacparkire.github.io/Ladiesden"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
