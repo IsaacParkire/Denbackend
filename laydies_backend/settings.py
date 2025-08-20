30455adf0edec762db1bdf7f50106e4a81f87700
IsaacParkire <pixieparkire@gmail.com>
2025-08-20 21:25:28 +0300

    Update to compatible package versions

--- laydies_backend/settings.py
+++ laydies_backend/settings.py
@@ -246,3 +246,8 @@ ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
+
+# at the bottom:
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
